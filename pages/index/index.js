70272b65e79e25766a6631d099cae813d670b9ce
1101099728@qq.com <142xyh753869>
2021-03-05 15:50:11 +0800

    优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,8 @@
 // index.js
 // 获取应用实例
+import DataBus from '../../lib/databus'
 import Main from '../../lib/main'
+const dataBus = new DataBus()
 const app = getApp()
 Page({
   data: {
@@ -17,15 +19,14 @@ Page({
     .fields({ node: true, size: true })
     .exec((res) => {
       const canvas = res[0].node
-      canvas.width = res[0].width
-      canvas.height = res[0].height
-      // const ctx = canvas.getContext('2d')
-      //
-      // const dpr = wx.getSystemInfoSync().pixelRatio
-      // canvas.width = res[0].width * dpr
-      // canvas.height = res[0].height * dpr
-      // ctx.scale(dpr, dpr)
+      const ctx = canvas.getContext('2d')
 
+      const dpr = wx.getSystemInfoSync().pixelRatio
+      canvas.width = res[0].width * dpr
+      canvas.height = res[0].height * dpr
+      ctx.scale(dpr, dpr)
+
+      dataBus.reset(res[0].width, res[0].height)
       new Main(canvas)
     })
     wx.showShareMenu()
